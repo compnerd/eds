5d70cbb5f2049b78d4063758f34fa7b41224c9fb
Saleem Abdulrasool <compnerd@compnerd.org>
2011-02-05 23:18:32 -0800

    parse-edid: pretty print the parsed edid data

--- src/examples/parse-edid/parse-edid.c
+++ src/examples/parse-edid/parse-edid.c
@@ -28,10 +28,20 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#define _GNU_SOURCE
+
+#include <math.h>
 #include <stdio.h>
+#include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <leds/edid.h>
+
+#define CM_2_MM(cm)                             ((cm) * 10)
+#define CM_2_IN(cm)                             ((cm) * 0.3937)
+
+#define ARRAY_SIZE(arr)                         (sizeof(arr) / sizeof((arr)[0]))
 
 static unsigned int
 get_block_count(FILE *edid)
@@ -195,6 +205,349 @@ dump_edid(const struct edid * const edid)
     printf("%40s %02x\n", "checksum:", edid->checksum);
 }
 
+static inline double
+_edid_fraction(uint16_t value)
+{
+    double result = 0.0;
+    uint8_t i;
+
+    assert(~value & 0xfc00);    /* edid fraction is 10 bits */
+
+    for (i = 0; value && (i < 10); i++, value >>= 1)
+        result += (value & 0x1) * (1.0 / (1 << (10 - i)));
+
+    return result;
+}
+
+static inline const char * const
+_aspect_ratio(const double hres, const double vres)
+{
+    const double ratio = hres / vres;
+
+    if (ratio == (16.0 / 10.0))
+        return "16:10";
+    if (ratio == (4.0 / 3.0))
+        return "4:3";
+    if (ratio == (5.0 / 4.0))
+        return "5:4";
+    if (ratio == (16.0 / 9.0))
+        return "16:9";
+
+    return "unknown";
+}
+
+static inline char *
+_edid_timing_string(const struct edid_detailed_timing_descriptor * const dtb)
+{
+    char *timing = NULL;
+    const uint16_t hres = edid_timing_horizontal_active(dtb);
+    const uint16_t vres = edid_timing_vertical_active(dtb);
+    const uint32_t htotal = hres + edid_timing_horizontal_blanking(dtb);
+    const uint32_t vtotal = vres + edid_timing_vertical_blanking(dtb);
+
+    asprintf(&timing,
+             "%ux%u%c at %.fHz (%s)",
+             hres,
+             vres,
+             dtb->interlaced ? 'i' : 'p',
+             (double) edid_timing_pixel_clock(dtb) / (vtotal * htotal),
+             _aspect_ratio(hres, vres));
+
+    return timing;
+}
+
+static inline char *
+_edid_mode_string(const struct edid_detailed_timing_descriptor * const dtb)
+{
+    char *modestr = NULL;
+    const uint16_t xres = edid_timing_horizontal_active(dtb);
+    const uint16_t yres = edid_timing_vertical_active(dtb);
+    const uint32_t pixclk = edid_timing_pixel_clock(dtb);
+    const uint16_t lower_margin = edid_timing_vertical_sync_offset(dtb);
+    const uint16_t right_margin = edid_timing_horizontal_sync_offset(dtb);
+
+    asprintf(&modestr,
+             "\"%ux%u\" %.3f %u %u %u %u %u %u %u %u %chsync %cvsync",
+             /* resolution */
+             xres, yres,
+
+             /* doct clock frequence (MHz) */
+             pixclk / 1000000.0,
+
+             /* horizontal timings */
+             xres,
+             xres + right_margin,
+             xres + right_margin + edid_timing_horizontal_sync_pulse_width(dtb),
+             xres + edid_timing_horizontal_blanking(dtb),
+
+             /* vertical timings */
+             yres,
+             yres + lower_margin,
+             yres + lower_margin + edid_timing_vertical_sync_pulse_width(dtb),
+             yres + edid_timing_vertical_blanking(dtb),
+
+             /* sync direction */
+             dtb->signal_pulse_polarity ? '+' : '-',
+             dtb->signal_serration_polarity ? '+' : '-');
+
+    return modestr;
+}
+
+static void
+parse_edid(const struct edid * const edid)
+{
+    char manufacturer[4] = {0};
+    char monitor_model_name[14] = {0};
+    char monitor_serial_number[14] = {0};
+    const struct edid_monitor_range_limits *monitor_range_limits = NULL;
+    struct edid_color_characteristics_data characteristics;
+    const uint8_t vlen = edid->maximum_vertical_image_size;
+    const uint8_t hlen = edid->maximum_horizontal_image_size;
+    uint8_t i;
+
+    static const char * const display_type[] =
+    {
+        [EDID_DISPLAY_TYPE_MONOCHROME] = "Monochrome or greyscale",
+        [EDID_DISPLAY_TYPE_RGB]        = "sRGB colour",
+        [EDID_DISPLAY_TYPE_NON_RGB]    = "Non-sRGB colour",
+        [EDID_DISPLAY_TYPE_UNDEFINED]  = "Undefined",
+    };
+
+    edid_manufacturer(edid, manufacturer);
+    characteristics = edid_color_characteristics(edid);
+
+
+    for (i = 0; i < ARRAY_SIZE(edid->detailed_timings); i++) {
+        const struct edid_monitor_descriptor * const desc =
+            &edid->detailed_timings[i].monitor;
+
+        if (desc->flag0 != 0x0000 || desc->flag1 != 0x00 || desc->flag2 != 0x00)
+            continue;
+
+        switch (desc->tag) {
+        case EDID_MONITOR_DESCRIPTOR_MONITOR_NAME:
+            strncpy(monitor_model_name, (char *) desc->data,
+                    sizeof(monitor_model_name) - 1);
+            *strchrnul(monitor_model_name, '\n') = '\0';
+            break;
+        case EDID_MONITOR_DESCRIPTOR_MONITOR_RANGE_LIMITS:
+            monitor_range_limits = (struct edid_monitor_range_limits *) &desc->data;
+            break;
+        case EDID_MONITOR_DESCRIPTOR_MONITOR_SERIAL_NUMBER:
+            strncpy(monitor_serial_number, (char *) desc->data,
+                    sizeof(monitor_serial_number) - 1);
+            *strchrnul(monitor_serial_number, '\n') = '\0';
+            break;
+        default:
+            fprintf(stderr, "unknown monitor descriptor type 0x%02x\n",
+                    desc->tag);
+            break;
+        }
+    }
+
+    printf("Monitor\n");
+
+    printf("  Model name............... %s\n",
+           monitor_model_name);
+
+    printf("  Manufacturer............. %s\n",
+           manufacturer);
+
+    printf("  Product code............. %u\n",
+           *(uint16_t *) edid->product);
+
+    printf("  Product serial........... %u\n",
+           *(uint32_t *) edid->serial_number);
+
+#if defined(DISPLAY_UNKNOWN)
+    printf("  Plug and Play ID......... %s\n", NULL);
+#endif
+
+    printf("  Serial number............ %s\n",
+           monitor_serial_number);
+
+    printf("  Manufacture date......... %u, ISO week %u\n",
+           edid->manufacture_year + 1990, edid->manufacture_week);
+
+    printf("  EDID revision............ %u.%u\n",
+           edid->version, edid->revision);
+
+    printf("  Input signal type........ %s\n",
+           edid->video_input_definition.digital.digital ? "Digital" : "Analog");
+
+    if (edid->video_input_definition.digital.digital) {
+        printf("  VESA DFP 1.x supported... %s\n",
+               edid->video_input_definition.digital.dfp_1x ? "Yes" : "No");
+    } else {
+        /* TODO print analog flags */
+    }
+
+#if defined(DISPLAY_UNKNOWN)
+    printf("  Color bit depth.......... %s\n", NULL);
+#endif
+
+    printf("  Display type............. %s\n",
+           display_type[edid->feature_support.display_type]);
+
+    printf("  Screen size.............. %u mm x %u mm (%.1f in)\n",
+           CM_2_MM(hlen), CM_2_MM(vlen),
+           CM_2_IN(sqrt(hlen * hlen + vlen * vlen)));
+
+    printf("  Power management......... %s%s%s\n",
+           edid->feature_support.active_off ? "Active off " : "",
+           edid->feature_support.suspend ? "Suspend " : "",
+           edid->feature_support.standby ? "Standby " : "");
+
+    printf("  Extension blocks......... %u\n",
+           edid->extensions);
+
+#if defined(DISPLAY_UNKNOWN)
+    printf("  DDC/CI................... %s\n", NULL);
+#endif
+
+    printf("\n");
+
+    printf("Color characteristics\n");
+
+    printf("  Default color space...... %ssRGB\n",
+           edid->feature_support.standard_default_color_space ? "" : "Non-");
+
+    printf("  Display gamma............ %.2f\n",
+           edid_gamma(edid));
+
+    printf("  Red chromaticity......... Rx %0.3f - Ry %0.3f\n",
+           _edid_fraction(characteristics.red.x),
+           _edid_fraction(characteristics.red.y));
+
+    printf("  Green chromaticity....... Gx %0.3f - Gy %0.3f\n",
+           _edid_fraction(characteristics.green.x),
+           _edid_fraction(characteristics.green.y));
+
+    printf("  Blue chromaticity........ Bx %0.3f - By %0.3f\n",
+           _edid_fraction(characteristics.blue.x),
+           _edid_fraction(characteristics.blue.y));
+
+    printf("  White point (default).... Wx %0.3f - Wy %0.3f\n",
+           _edid_fraction(characteristics.white.x),
+           _edid_fraction(characteristics.white.y));
+
+#if defined(DISPLAY_UNKNOWN)
+    printf("  Additional descriptors... %s\n", NULL);
+#endif
+
+    printf("\n");
+
+    printf("Timing characteristics\n");
+
+    if (monitor_range_limits) {
+        printf("  Horizontal scan range.... %u - %u kHz\n",
+               monitor_range_limits->minimum_horizontal_rate,
+               monitor_range_limits->maximum_horizontal_rate);
+
+        printf("  Vertical scan range...... %u - %u Hz\n",
+               monitor_range_limits->minimum_vertical_rate,
+               monitor_range_limits->maximum_vertical_rate);
+
+        printf("  Video bandwidth.......... %u MHz\n",
+               monitor_range_limits->maximum_supported_pixel_clock * 10);
+    }
+
+#if defined(DISPLAY_UNKNOWN)
+    printf("  CVT standard............. %s\n", NULL);
+#endif
+
+    printf("  GTF standard............. %sSupported\n",
+           edid->feature_support.default_gtf ? "" : "Not ");
+
+#if defined(DISPLAY_UNKNOWN)
+    printf("  Additional descriptors... %s\n", NULL);
+#endif
+
+    printf("  Preferred timing......... %s\n",
+           edid->feature_support.preferred_timing_mode ? "Yes" : "No");
+
+    if (edid->feature_support.preferred_timing_mode) {
+        char *string = NULL;
+
+        string = _edid_timing_string(&edid->detailed_timings[0].timing);
+        printf("  Native/preferred timing.. %s\n", string);
+        free(string);
+
+        string = _edid_mode_string(&edid->detailed_timings[0].timing);
+        printf("    Modeline............... %s\n", string);
+        free(string);
+    } else {
+        printf("  Native/preferred timing.. n/a\n");
+    }
+
+    printf("\n");
+
+    printf("Standard timings supported\n");
+    if (edid->established_timings.timing_720x400_70)
+        printf("   720 x  400p @ 70Hz - IBM VGA\n");
+    if (edid->established_timings.timing_720x400_88)
+        printf("   720 x  400p @ 88Hz - IBM XGA2\n");
+    if (edid->established_timings.timing_640x480_60)
+        printf("   640 x  480p @ 60Hz - IBM VGA\n");
+    if (edid->established_timings.timing_640x480_67)
+        printf("   640 x  480p @ 67Hz - Apple Mac II\n");
+    if (edid->established_timings.timing_640x480_72)
+        printf("   640 x  480p @ 72Hz - VESA\n");
+    if (edid->established_timings.timing_640x480_75)
+        printf("   640 x  480p @ 75Hz - VESA\n");
+    if (edid->established_timings.timing_800x600_56)
+        printf("   800 x  600p @ 56Hz - VESA\n");
+    if (edid->established_timings.timing_800x600_60)
+        printf("   800 x  600p @ 60Hz - VESA\n");
+
+    if (edid->established_timings.timing_800x600_72)
+        printf("   800 x  600p @ 72Hz - VESA\n");
+    if (edid->established_timings.timing_800x600_75)
+        printf("   800 x  600p @ 75Hz - VESA\n");
+    if (edid->established_timings.timing_832x624_75)
+        printf("   832 x  624p @ 75Hz - Apple Mac II\n");
+    if (edid->established_timings.timing_1024x768_87)
+        printf("  1024 x  768i @ 87Hz - VESA\n");
+    if (edid->established_timings.timing_1024x768_60)
+        printf("  1024 x  768p @ 60Hz - VESA\n");
+    if (edid->established_timings.timing_1024x768_70)
+        printf("  1024 x  768p @ 70Hz - VESA\n");
+    if (edid->established_timings.timing_1024x768_75)
+        printf("  1024 x  768p @ 75Hz - VESA\n");
+    if (edid->established_timings.timing_1280x1024_75)
+        printf("  1280 x 1024p @ 75Hz - VESA\n");
+
+    for (i = 0; i < ARRAY_SIZE(edid->standard_timing_id); i++) {
+        const struct edid_standard_timing_descriptor * const desc =
+            &edid->standard_timing_id[i];
+
+        uint16_t hres, vres;
+
+        if (!memcmp(desc, EDID_STANDARD_TIMING_DESCRIPTOR_INVALID, sizeof(*desc)))
+            continue;
+
+        hres = (desc->horizontal_active_pixels + 31) << 3;
+
+        switch (desc->image_aspect_ratio) {
+        case EDID_ASPECT_RATIO_16_10:
+            vres = (hres * (10.0 / 16.0));
+            break;
+        case EDID_ASPECT_RATIO_4_3:
+            vres = (hres * (3.0 / 4.0));
+            break;
+        case EDID_ASPECT_RATIO_5_4:
+            vres = (hres * (4.0 / 5.0));
+            break;
+        case EDID_ASPECT_RATIO_16_9:
+            vres = (hres * (9.0 / 16.0));
+            break;
+        }
+
+        printf("  %4u x %4u%c @ %uHz - VESA STD\n",
+               hres, vres, 'p', desc->refresh_rate + 60);
+    }
+}
+
 static void
 dump_edid_data(const uint8_t * const data)
 {
@@ -204,6 +557,8 @@ dump_edid_data(const uint8_t * const data)
         (struct edid_extension *) (data + sizeof(*edid));
 
     dump_edid(edid);
+    printf("\n");
+    parse_edid(edid);
 
     printf("\n");
 
